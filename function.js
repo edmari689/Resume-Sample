9ad142039ea7b2fee4e8461bea00b13d390db672
Edmari Callangan <edmari689@yahoo.com>
2020-10-19 15:57:57 +0900

    Removed unnecessary codes

--- function.js
+++ function.js
@@ -6,29 +6,6 @@ function scrollingTop() {
   }
 }
 
-/*
-var scrollToTopBtn = document.querySelector(".topButton");
-var rootElement = document.documentElement;
-
-function handleScroll() {
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
-    scrollToTopBtn.style.display = "block";
-  } else {
-    scrollToTopBtn.style.display = "none";
-  }
-}
-
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop);
-document.addEventListener("scroll", handleScroll);
-*/
-
 $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
