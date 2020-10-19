597c039390b1082a3ebd22b2d042955a8e205a32
Edmari Callangan <edmari689@yahoo.com>
2020-10-19 16:58:22 +0900

    Changed and adjusted codes for scrolling event

--- function.js
+++ function.js
@@ -1,10 +1,18 @@
-function scrollingTop() {
-  const y = window.scrollY;
-  if(y > 0){
-    window.requestAnimationFrame(scrollingTop);
-    window.scrollTo(0, y - y / 30);
-  }
+//Section for scrolling to the top
+function scrollTo(element) {
+  window.scroll({
+    behavior: 'smooth',
+	left: 0,
+    top: element.offsetTop //offsetTop returns the top position (in pixels) relative to the top of the offsetParent element
+  });
 }
+
+var goUp = document.querySelector(".topLink"); 
+var topHead = document.getElementById("headArea");
+goUp.addEventListener('click', () => {
+  scrollTo(topHead); //goes to the specific div by clicking the link
+});
+
 
 $(window).on("load",function() {
   $(window).scroll(function() {
