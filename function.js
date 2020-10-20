582abec0815f1a0a36ca3f4d91c3072386a1d14f
Edmari Callangan <edmari689@yahoo.com>
2020-10-20 16:14:22 +0900

    Removed codes for fade-in animation with JQuery library

--- function.js
+++ function.js
@@ -12,17 +12,3 @@ var topHead = document.getElementById("headArea");
 goUp.addEventListener('click', () => {
   scrollTo(topHead); //goes to the specific div by clicking the link
 });
-
-
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".toFadeIn").each(function() {
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-      if (objectBottom < windowBottom) { 
-        if ($(this).css("opacity")==0) {$(this).fadeTo(1000,1);}
-      } 
-    });
-  }).scroll(); 
-});
